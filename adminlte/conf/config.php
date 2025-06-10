6b3b78c5863751a01afe8c21308f4300b77705d1
ahmad-imtiyaz <imtiyaznajih8@gmail.com>
2025-06-10 22:08:59 +0700

    menambahkan sisanya

---
+++ adminlte/conf/config.php
@@ -0,0 +1,9 @@
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "db_lte");
+
+// // Check connection
+// if (!$koneksi) {
+//     die("Connection failed: " . mysqli_connect_error());
+// } else {
+//     // echo "Connected successfully";
+// }
