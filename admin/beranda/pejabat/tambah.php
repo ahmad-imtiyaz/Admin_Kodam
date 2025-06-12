6c35712096317aa217fb2d57f66a422e52ab8ae0
ahmad-imtiyaz <imtiyaznajih8@gmail.com>
2025-06-12 12:41:15 +0700

    mulai bagi tugas sesama

--- admin/beranda/pejabat/tambah.php
+++ admin/beranda/pejabat/tambah.php
@@ -17,33 +17,38 @@ if (isset($_POST['simpan'])) {
     $jabatan  = $_POST['jabatan'];
     $twitter  = $_POST['twitter'];
     $facebook = $_POST['facebook'];
-    $instagram= $_POST['instagram'];
+    $instagram = $_POST['instagram'];
     $linkedin = $_POST['linkedin'];
 
-    $foto_url = $_FILES['foto_url']['name'];
-    $target_dir = "../../../kodam/img/";
-    $target_file = $target_dir . basename($foto_url);
+    $foto_name = $_FILES['foto_url']['name'];
+    $foto_tmp  = $_FILES['foto_url']['tmp_name'];
+    $target_dir = "img/";
+    $target_file = $target_dir . basename($foto_name);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    // Validasi file
-    if ($_FILES['foto_url']['size'] > 20000000000000000000) {
+    // Validasi ukuran file maksimal 2MB
+    if ($_FILES['foto_url']['size'] > 2000000) {
         echo "<script>alert('Ukuran file terlalu besar. Maksimal 2MB.');</script>";
         $uploadOk = 0;
     }
+
+    // Validasi ekstensi file
     if (!in_array($imageFileType, ['jpg', 'jpeg', 'png'])) {
         echo "<script>alert('Hanya file JPG, JPEG, dan PNG yang diperbolehkan.');</script>";
         $uploadOk = 0;
     }
 
-    if ($uploadOk && move_uploaded_file($_FILES['foto_url']['tmp_name'], $target_file)) {
+    // Jika semua valid, lanjut simpan file dan data
+    if ($uploadOk && move_uploaded_file($foto_tmp, $target_file)) {
+        // Simpan path relatif (misalnya: img/foto.jpg)
         $sql = "INSERT INTO pejabat (nama, jabatan, foto_url, twitter_link, facebook_link, instagram_link, linkedin) 
                 VALUES ('$nama', '$jabatan', '$target_file', '$twitter', '$facebook', '$instagram', '$linkedin')";
 
         if ($koneksi->query($sql)) {
             echo "<script>alert('Data berhasil ditambahkan'); window.location='pejabat.php';</script>";
         } else {
-            echo "Error: " . $koneksi->error;
+            echo "Error saat menyimpan ke database: " . $koneksi->error;
         }
     } else {
         echo "<script>alert('Gagal upload foto.');</script>";
